52ab1a149ad705a3bd71623a5bbbc89a4a15afb9
Elana Ryznar <eryzna@gmail.com>
2019-10-07 05:10:42 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,12 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "fwitter_secret"
   end
 
   get '/' do
-    erb :'/index'
+    erb :index
   end
 
   helpers do
@@ -20,7 +22,6 @@ class ApplicationController < Sinatra::Base
     def current_user
       @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
     end
+
   end
-
-
 end
